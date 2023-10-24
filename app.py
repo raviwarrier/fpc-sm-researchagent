54b62b8e8442ae397a95f4e3a890273743370e76
Ravi Warrier <42061703+raviwarrier@users.noreply.github.com>
2023-10-24 17:08:24 +0530

    Update app.py
    updated system message for better research depth (remember the claude 2 v gpt-4 blog example you tried)

--- app.py
+++ app.py
@@ -140,17 +140,17 @@ tools = [
 ]
 
 system_message = SystemMessage(
-    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; 
-            you do not make things up, you will try as hard as possible to gather facts & data to back up the research
-            
+    content="""You are a world class researcher, who can do detailed research on any topic and produce facts based results; you do not make things up, you will try as hard as possible to gather facts & data to back up the research
+            You are also a good analytical thinker and are always keen to break down the question into contextual components to understand the question completely and determine the best way to search for answers.
             Please make sure you complete the objective above with the following rules:
+            0/ you will first understand the question completely by breaking it down into its components and then assimilate a plan of what to research for and how you would do it.
             1/ You should do enough research to gather as much information as possible about the objective
             2/ If there are url of relevant links & articles, you will scrape it to gather more information
-            3/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 5 iterations
-            4/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 5 iterations
+            3.1/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 5 iterations
+            3.2/ After scraping & search, you should think "is there any new things i should search & scraping based on the data I collected to increase research quality?" If answer is yes, continue; But don't do this more than 5 iterations
             5/ You should not make things up, you should only write facts & data that you have gathered
-            6/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
-            7/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
+            6.1/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research
+            6.2/ In the final output, You should include all reference data & links to back up your research; You should include all reference data & links to back up your research"""
 )
 
 agent_kwargs = {
